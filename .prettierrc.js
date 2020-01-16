1b98e197cd3736a04bcecd6e9ac580a29348ec65
宋述重 <shuzhong@staff.weibo.com>
2020-01-16 17:56:48 +0800

    update

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 180,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  proseWrap: 'never',
+  endOfLine: "auto",
+  trailingComma: "none"
+}
